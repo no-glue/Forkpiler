726ce9d67db8a6b89d990017f8e04bb3c7fe8243
Thomas Crescenzi <trescenzi@gmail.com>
2013-02-15 10:48:28 -0500

    folds both set to be tested

--- Lexer.hs
+++ Lexer.hs
@@ -5,43 +5,33 @@ import Text.Regex.Posix
 import Data.List
 
 lex :: String -> [Token]
-lex input = do
-  breakLines = lines input
-  foldl (\++) [] (foldl (++) []  
+lex input =
+  foldl (\++ processLine) [] breakLines   
+  where breakLines = lines input
 
 processLine :: [String] -> [Token]
 processLine line = 
-  foldl (\++) [] 
+  foldl (\++ processWord) [] line
 
-processWord :: String -> Int -> [Token]
+processWord :: String -> Token
 processWord input 
   |input =~ quotation :: Bool =
-    let token = Token (input =~ quotation :: String) (lineNumber,0) "quotation" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ quotation :: String) (0,0) "quotation" 
   |input =~ parenOpen :: Bool = 
-    let token = Token (input =~ parenOpen :: String) (lineNumber,0) "parenOpen" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ parenOpen :: String) (0,0) "parenOpen" 
   |input =~ parenClose :: Bool = 
-    let token = Token (input =~ parenClose :: String) (lineNumber,0) "parenClose" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ parenClose :: String) (0,0) "parenClose" 
   |input =~ equalsOp :: Bool = 
-    let token = Token (input =~ equalsOp :: String) (lineNumber,0) "equalsOp" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ equalsOp :: String) (0,0) "equalsOp" 
   |input =~ plusOp :: Bool = 
-    let token = Token (input =~ plusOp :: String) (lineNumber,0) "plusOp" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ plusOp :: String) (0,0) "plusOp" 
   |input =~ minusOp :: Bool =
-    let token = Token (input =~ minusOp :: String) (lineNumber,0) "minusOp" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ minusOp :: String) (0,0) "minusOp" 
   |input =~ openBrace :: Bool =
-    let token = Token (input =~ openBrace :: String) (lineNumber,0) "openBrace" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ openBrace :: String) (0,0) "openBrace" 
   |input =~ closeBrace :: Bool =
-    let token = Token (input =~ closeBrace :: String) (lineNumber,0) "closeBrace" 
-      in token : lexer (delete (contents token) input) lineNumber
+    Token (input =~ closeBrace :: String) (0,0) "closeBrace" 
   where 
-    whitespace = "^[ \t\n]"
-    endline = "^[\n]"
     quotation = "^[\"]"
     parenOpen = "^[(]"
     parenClose = "^[)]"

661e06f99adafdf27033cbc7051e1f1f7083636e
Thomas Crescenzi <trescenzi@gmail.com>
2013-02-23 14:45:20 -0500

    never added main. oppsies

---
+++ Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Lexer
+import System.Environment
+
+main = do
+  [inFile] <- getArgs
+  rawCode <- readFile inFile
+  let tokens = Lexer.lex $ rawCode
+  debugPrint tokens
+  writeFile "output.txt" (show tokens) 

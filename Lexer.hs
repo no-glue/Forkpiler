f49709c7157b207cb4b5b8e3d105d84fdb464e3d
Thomas Crescenzi <trescenzi@gmail.com>
2013-02-23 11:02:31 -0500

    changing around how lexer works... again

--- Lexer.hs
+++ Lexer.hs
@@ -4,79 +4,78 @@ import Token
 import Text.Regex.PCRE
 import Data.List
 
-lex :: String -> [Token]
+lex :: String -> [(Token,Token)]
 lex =
   checkEnd . processFile
 
-checkEnd :: [Token] -> [Token]
+checkEnd :: [(Token,Token)] -> [(Token,Token)]
 checkEnd [] = []
 checkEnd tokens 
-  |end == Error = tokens
-  |end == Warning = tokens
   |kind end == kind dollarToken = tokens
-  |otherwise = error("No dollar sign at end of program") 
+  |otherwise = (Token "No $ found added $" (0,0) "lex warning", 
+               dollarToken ) : tokens 
   where 
-    end = last tokens
+    end = fst (last tokens)
     dollarToken = Token "$" (0,0) "eof"
 
-processFile :: String -> [Token]
+processFile :: String -> [(Token,Token)]
 processFile file = 
   let fileLines = lines file
   in foldr (\ line next -> processLine line ++ next) [] fileLines
 
-processLine :: String -> [Token]
+processLine :: String -> [(Token,Token)]
 processLine line = 
   let brokenLine = words line
   in foldr (\ word next -> processWord word ++ next) [] brokenLine
 
-processWord :: String -> [Token]
+processWord :: String -> [(Token,Token)]
 processWord [] = []
 processWord input 
   |input =~ characterList :: Bool =
     let token = Token (input =~ characterList :: String) (0,0) "charList"
-    in token : (processWord (input \\ (contents token))) 
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ parenOpen :: Bool = 
-    let token = Token (input =~ parenOpen :: String) (0,0) "parenOpen" 
-    in token : (processWord (input \\ (contents token))) 
+    let token = Token (input =~ parenOpen :: String) (0,0) "parenOpen"
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ parenClose :: Bool = 
     let token = Token (input =~ parenClose :: String) (0,0) "parenClose"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ equalsOp :: Bool = 
     let token = Token (input =~ equalsOp :: String) (0,0) "equalsOp"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ plusOp :: Bool = 
     let token = Token (input =~ plusOp :: String) (0,0) "plusOp"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ minusOp :: Bool =
     let token = Token (input =~ minusOp :: String) (0,0) "minusOp" 
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ openBrace :: Bool =
     let token = Token (input =~ openBrace :: String) (0,0) "openBrace" 
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ closeBrace :: Bool =
     let token = Token (input =~ closeBrace :: String) (0,0) "closeBrace" 
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ digit :: Bool =
     let token = Token (input =~ digit :: String) (0,0) "digit"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ printOp :: Bool = 
     let token = Token (input =~ printOp :: String) (0,0) "print"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ int :: Bool = 
     let token = Token (input =~ int :: String) (0,0) "int"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ char :: Bool = 
     let token = Token (input =~ char :: String) (0,0) "char"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ identifier :: Bool = 
     let token = Token (input =~ identifier :: String) (0,0) "id"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |input =~ eof :: Bool = 
     let token = Token (input =~ eof :: String) (0,0) "eof"
-    in token : (processWord (input \\ (contents token)))
+    in buildTokens token : processWord (input \\ (contents token))
   |otherwise = 
-    let token = Error "lex error with:"
-    in token : []
+    let token = Token input (0,0) "lex error"
+    in buildTokens token : [] 
   where 
     parenOpen = "^[(]"
     parenClose = "^[)]"
@@ -92,3 +91,8 @@ processWord input
     char = "^char"
     identifier = "^[a-zA-Z]$"
     eof = "^\\$$"
+
+--debugPrint :: Token -> Token
+debugPrint (x:xs) = do
+  putStrLn ("lexing " ++ (contents x) ++ " as " ++ (kind x)) 
+  debugPrint xs 
--- parseTypes.hs
+++
@@ -1,7 +0,0 @@
-module ParseTypes(Statement, Exper, End) where
-
-data Statement = Statement | Exper End 
-
-data Exper = Int | Char
-
-data End = Monday deriving(Enum)

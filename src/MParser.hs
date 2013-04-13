dedc81332d2a766a414dba376c878cc43df7685a
Thomas Crescenzi <trescenzi@gmail.com>
2013-04-13 16:38:35 -0400

    cst coming along nicely. all terminals added in

--- src/MParser.hs
+++ src/MParser.hs
@@ -19,29 +19,33 @@ statement :: TokenCST -> TokenCST
 statement ((token:rest), cst) =
   case (kind token) of
     PrintOp -> do
-      let remaining = trace("Parsing print op") consumeToken ParenOpen (rest,cst)
+      let remaining = trace("Parsing print op") consumeToken ParenOpen (rest,cst2)
       let follow = exper remaining
+     -- let (done, cst3) = 
       consumeToken ParenClose follow
+      --(done, addParentNode cst3 Statement)
     ID -> do
-      let remaining = trace("Parsing Id expression") consumeToken EqualsOp (rest,cst)
+      let remaining = trace("Parsing Id expression") consumeToken EqualsOp (rest,cst2)
       exper remaining
-    IntOp -> trace("Parsing int decleration") varDecl (rest,cst)
-    CharOp -> trace("Parsing char decleration") varDecl (rest,cst)
+    IntOp -> trace("Parsing int decleration") varDecl (rest,cst2)
+    CharOp -> trace("Parsing char decleration") varDecl (rest,cst2)
     OpenBrace -> do
-      let remaining = trace("Parsing statementList") statementList (rest,cst)
+      let remaining = trace("Parsing statementList") statementList (rest,cst2)
       consumeToken CloseBrace remaining
     _ -> unexpected token 
+    where cst2 = addChildNode cst (Terminal token)
 
 exper :: TokenCST -> TokenCST 
 exper ([], _) = error("Error: Found nothing -- Expected digit, " ++
                  "string expression or ID in expr")
 exper ((token:rest), cst) =
   case (kind token) of
-    Digit -> intExper (rest,cst)
+    Digit -> intExper (rest,cst2)
     CharacterList ->
-      trace("Parsed character list") (rest,cst)
-    ID -> trace("Parsed ID") (rest,cst)
+      trace("Parsed character list") (rest,cst2)
+    ID -> trace("Parsed ID") (rest,cst2)
     _ -> unexpected token
+    where cst2 = addChildNode cst (Terminal token)
 
 varDecl :: TokenCST -> TokenCST 
 varDecl ([],_) = error("Error: Found nothing -- Expected ID in variable decleration")
@@ -52,20 +56,22 @@ statementList :: TokenCST -> TokenCST
 statementList ([],_) = statementError 
 statementList ((token:rest),cst)
   --on finding follow of statementList epislon
-  |tt == CloseBrace = ((token:rest),cst)
+  |tt == CloseBrace = ((token:rest),cst2)
   --on finding first of statement do the statementList thing
   |tt == PrintOp || tt == ID || tt == IntOp || tt == CharOp || tt == OpenBrace = do
-    let remaining = statement ((token:rest), cst)
+    let remaining = statement ((token:rest), cst2)
     statementList remaining
   --otherwise you done screwed up
   |otherwise = unexpected token
   where tt = kind token 
+        cst2 = addChildNode cst (Terminal token)
 
 intExper :: TokenCST -> TokenCST 
 intExper ([],cst) = ([],cst)
 intExper ((token:rest),cst) =
   case (kind token) of
-    PlusOp -> exper (rest,cst)
-    MinusOp -> exper (rest,cst) 
+    PlusOp -> exper (rest,cst2)
+    MinusOp -> exper (rest,cst2) 
     --epislon in intExper because it is entered upon detection of a digit
     _ -> ((token:rest),cst)
+    where cst2 = addChildNode cst (Terminal token)
